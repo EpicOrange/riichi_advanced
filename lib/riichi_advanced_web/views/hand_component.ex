b04cbdbae7e571e7a512a00bc8a507ce5f3cc5f0
Daniel Wang <danielwang9@protonmail.com>
2024-08-17 15:50:40 -0500

    Hide opponent tiles

--- lib/riichi_advanced_web/views/hand_component.ex
+++ lib/riichi_advanced_web/views/hand_component.ex
@@ -42,7 +42,7 @@ defmodule RiichiAdvancedWeb.HandComponent do
           <% end %>
         </div>
         <div class="draws">
-          <%= for tile <- @draw do %>
+          <%= for {tile, _i} <- prepare_draw(assigns) do %>
             <div class={["tile", tile]}></div>
           <% end %>
         </div>
@@ -70,9 +70,14 @@ defmodule RiichiAdvancedWeb.HandComponent do
     {:noreply, socket}
   end
 
+  def hide_tiles(tiles, assigns) do
+    if assigns.your_hand? do tiles else Enum.map(tiles, fn _tile -> :"1x" end) end
+  end
+
   def prepare_hand(assigns) do
     # map tiles to [{index, tile, is_last_discard}]
     assigns.hand
+      |> hide_tiles(assigns)
       |> Enum.with_index
       |> Enum.map(fn {tile, i} -> {if assigns.played_tile_index != nil && i >= assigns.played_tile_index do i-1 else i end, tile, i == assigns.played_tile_index} end)
   end
@@ -80,7 +85,9 @@ defmodule RiichiAdvancedWeb.HandComponent do
   def prepare_draw(assigns) do
     # map tiles to [{index, tile}]
     # this function is necessary since we need phoenix to update draws when anything in assigns changes
-    assigns.draw |> Enum.with_index
+    assigns.draw
+      |> hide_tiles(assigns)
+      |> Enum.with_index
   end
 
   def update(assigns, socket) do
@@ -92,6 +99,11 @@ defmodule RiichiAdvancedWeb.HandComponent do
     # this gets undone after 750ms
     socket = if Map.has_key?(socket.assigns, :played_tile_index) do
       socket = if socket.assigns.played_tile_index != nil do
+        # randomize position of played tile (if tedashi)
+        socket = if not socket.assigns.your_hand? && socket.assigns.played_tile_index < length(socket.assigns.hand) do
+          assign(socket, :played_tile_index, Enum.random(1..length(socket.assigns.hand)) - 1)
+          else socket end
+
         actual_hand = socket.assigns.hand
         socket = assign(socket, :hand, List.insert_at(actual_hand, socket.assigns.played_tile_index, socket.assigns.played_tile))
         :timer.apply_after(750, Kernel, :send, [self(), {:reset_anim, actual_hand, socket.assigns.seat}])
