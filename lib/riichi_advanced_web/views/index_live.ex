6442a10447f13dbcf8a0fc309f9c8f060277650d
Daniel Wang <danielwang9@protonmail.com>
2024-08-17 21:40:12 -0500

    Style index page

--- lib/riichi_advanced_web/views/index_live.ex
+++ lib/riichi_advanced_web/views/index_live.ex
@@ -2,13 +2,20 @@ defmodule RiichiAdvancedWeb.IndexLive do
   use RiichiAdvancedWeb, :live_view
 
   def mount(_params, _session, socket) do
+    socket = assign(socket, :session_id, "asdf")
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    <div class="form">
-      Hello World!
+    <div class="main-page">
+      <div class="title">
+        <div class="title-riichi">Riichi</div>
+        <div class="title-advanced">Advanced</div>
+        <div class="tile 8m"></div>
+        <div class="tile 7z"></div>
+      </div>
+      <.link href={~p"/game/#{@session_id}"} class="enter-button">Enter</.link>
     </div>
     """
   end
