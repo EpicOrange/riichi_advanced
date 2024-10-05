c5c893c93b10393705b0aa108bec0aaeae8b2669
Daniel Wang <danielwang9@protonmail.com>
2024-10-05 18:29:48 -0500

    Fix modloader

--- lib/riichi_advanced/mod_loader.ex
+++ lib/riichi_advanced/mod_loader.ex
@@ -13,11 +13,11 @@ defmodule RiichiAdvanced.ModLoader do
     modded_json = Enum.reduce(mod_names, ruleset_json, &apply_mod/2)
 
     # list out the mods as a "enabled_mods" key, via string replacement
-    modded_json = if String.contains?(ruleset_json, "\"") do
-      String.replace(ruleset_json, "{", "{\"enabled_mods\":" <> inspect(mod_names) <> ",", global: false)
+    modded_json = if String.contains?(modded_json, "\"") do
+      String.replace(modded_json, "{", "{\"enabled_mods\":" <> inspect(mod_names) <> ",", global: false)
     else
       # if the json file is empty, don't add the comma
-      String.replace(ruleset_json, "{", "{\"enabled_mods\":" <> inspect(mod_names), global: false)
+      String.replace(modded_json, "{", "{\"enabled_mods\":" <> inspect(mod_names), global: false)
     end
 
     modded_json
