a3019a5e810e7d7b72bb1bcfa77ad70898783ae4
Daniel Wang <danielwang9@protonmail.com>
2024-11-18 16:30:59 -0600

    Remove (beta) from Cosmic Riichi

--- lib/riichi_advanced_web/views/index_live.ex
+++ lib/riichi_advanced_web/views/index_live.ex
@@ -32,7 +32,7 @@ defmodule RiichiAdvancedWeb.IndexLive do
           <option value="saki">Sakicards v1.3</option>
           <option value="vietnamese">Vietnamese</option>
           <option value="bloody30faan">Bloody 30-Faan Jokers</option>
-          <option value="cosmic">Cosmic Riichi (beta)</option>
+          <option value="cosmic">Cosmic Riichi</option>
           <option value="custom">Custom</option>
         </select>
         <br/>
