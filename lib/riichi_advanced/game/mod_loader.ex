937c852ee1ef8b2f93299de38b273274b9b67501
Daniel Wang <danielwang9@protonmail.com>
2024-12-07 15:59:06 -0600

    Streamline mod loading

--- lib/riichi_advanced/game/mod_loader.ex
+++ lib/riichi_advanced/game/mod_loader.ex
@@ -1,12 +1,35 @@
 defmodule RiichiAdvanced.ModLoader do
   alias RiichiAdvanced.GameState.Debug, as: Debug
 
-  def apply_mod(mod_name, ruleset_json) do
+  def apply_single_mod(mod_name, ruleset_json) do
     # apply mods
     query_path = Application.app_dir(:riichi_advanced, "/priv/static/mods/#{mod_name <> ".jq"}")
     IO.puts("Applying mod #{mod_name}")
     ruleset_json = JQ.query_string!(ruleset_json, query_path)
     ruleset_json
+  end
+
+  def apply_multiple_mods(ruleset_json, mod_names) do
+    mod_contents = mod_names
+    |> Enum.map(&File.read!(Application.app_dir(:riichi_advanced, "/priv/static/mods/#{&1 <> ".jq"}")))
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&" | (#{&1}) as $_result\n|\n$_result")
+    |> Enum.join()
+    |> then(&"."<>&1)
+
+    IO.puts(mod_contents)
+
+    # write to path
+    {fd, query_path} = Temp.open!(%{mode: [:write, :utf8]})
+    IO.write(fd, mod_contents)
+    File.close(fd)
+
+    try do
+      IO.puts("Applying mods #{Enum.join(mod_names, ", ")}")
+      JQ.query_string!(ruleset_json, query_path)
+    after
+      File.rm!(query_path)
+    end
   end
 
   def apply_mods(ruleset, ruleset_json, mod_names) do
@@ -16,7 +39,8 @@ defmodule RiichiAdvanced.ModLoader do
         modded_json
       []     -> 
         # apply the mods
-        modded_json = Enum.reduce(mod_names, ruleset_json, &apply_mod/2)
+        # modded_json = Enum.reduce(mod_names, ruleset_json, &apply_mod/2)
+        modded_json = apply_multiple_mods(ruleset_json, mod_names)
 
         # list out the mods as a "enabled_mods" key, via string replacement
         modded_json = if String.contains?(modded_json, "\"") do
