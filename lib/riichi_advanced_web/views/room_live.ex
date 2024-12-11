e77b899d33368fcea6d480827fcca6d6831d16f4
Daniel Wang <danielwang9@protonmail.com>
2024-12-10 21:33:19 -0600

    Add toggle mod category button

--- lib/riichi_advanced_web/views/room_live.ex
+++ lib/riichi_advanced_web/views/room_live.ex
@@ -136,9 +136,12 @@ defmodule RiichiAdvancedWeb.RoomLive do
         <div class={["mods", "mods-#{@state.ruleset}"]}>
           <div class="mods-inner-container">
             <%= for {category, mods} <- Enum.group_by(@state.mods, fn {_name, mod} -> mod.category end) |> Enum.sort_by(fn {category, _mods} -> Enum.find_index(@state.categories, & &1 == category) end) do %>
-              <div class="mod-category"><%= category %></div>
+              <div class="mod-category">
+                <%= category %>
+                <button class="toggle-category-button" phx-cancellable-click="toggle_category" phx-value-category={category}>Toggle all</button>
+              </div>
               <%= for {mod, mod_details} <- Enum.sort_by(mods, fn {_name, mod} -> mod.index end) do %>
-                <input id={mod} type="checkbox" phx-click="toggle_mod" phx-value-mod={mod} phx-value-enabled={if @state.mods[mod].enabled do "true" else "false" end} checked={@state.mods[mod].enabled}>
+                <input id={mod} type="checkbox" phx-cancellable-click="toggle_mod" phx-value-mod={mod} phx-value-enabled={if @state.mods[mod].enabled do "true" else "false" end} checked={@state.mods[mod].enabled}>
                 <label for={mod} title={mod_details.desc} class={["mod", mod_details.class]}><%= mod_details.name %></label>
               <% end %>
               <div class="mod-category-spacer"></div>
@@ -197,6 +200,13 @@ defmodule RiichiAdvancedWeb.RoomLive do
   def handle_event("toggle_mod", %{"mod" => mod, "enabled" => enabled}, socket) do
     enabled = enabled == "true"
     GenServer.cast(socket.assigns.room_state, {:toggle_mod, mod, not enabled})
+    {:noreply, socket}
+  end
+
+  def handle_event("toggle_category", %{"category" => category}, socket) do
+    for {name, mod} <- socket.assigns.state.mods, mod.category == category do
+      GenServer.cast(socket.assigns.room_state, {:toggle_mod, name, not mod.enabled})
+    end
     {:noreply, socket}
   end
 
