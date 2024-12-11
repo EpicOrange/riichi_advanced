cb3032c4fac297dbfd53d932f5b1e330bfb70a09
Daniel Wang <danielwang9@protonmail.com>
2024-12-11 12:12:42 -0600

    Make clicking the compass show relative scores for 2 seconds

--- lib/riichi_advanced_web/views/compass_component.ex
+++ lib/riichi_advanced_web/views/compass_component.ex
@@ -2,27 +2,29 @@ defmodule RiichiAdvancedWeb.CompassComponent do
   use RiichiAdvancedWeb, :live_component
 
   def mount(socket) do
+    socket = assign(socket, :show_relative_scores, false)
+    socket = assign(socket, :show_relative_scores_timer, nil)
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    <div class={["compass", @seat]} phx-click="notify_ai" phx-target={@myself}>
+    <div class={["compass", @seat]} phx-click="compass_clicked" phx-target={@myself}>
       <div class="centerpiece">
         <div class="tiles-left"><%= @tiles_left %></div>
         <div class="riichi-stick-counter" :if={@display_riichi_sticks}><%= @riichi_sticks %></div>
         <div class="honba-counter" :if={@display_honba}><%= @honba %></div>
       </div>
-      <%= for {dir, symbol} <- prepare_compass(@kyoku, @available_seats) do %>
-        <div class="score-box"></div>
+      <div class="score-box"></div>
+      <%= for {dir, symbol, score} <- prepare_compass(assigns) do %>
         <div class={["direction", dir]}>
           <div class={["riichi-tray", @turn == dir && "highlighted", @riichi[dir] && "riichi"]}></div>
           <div class={["wind-marker", @turn == dir && "highlighted", @is_bot[dir] && "bot"]}><%= symbol %></div>
-          <div class="score-counter" :if={dir in @available_seats}>
-            <%= if abs(@score[dir]) >= 1000000 && @score_e_notation do %>
-              <%= 1.0 * @score[dir] |> :erlang.float_to_binary() |> String.slice(0..3) %>e<b><%= @score[dir] |> abs() |> :math.log10() |> trunc() %></b>
+          <div class={["score-counter", score < 0 && "negative", abs(score) >= 1000000 && "scientific"]} :if={dir in @available_seats}>
+            <%= if abs(score) >= 1000000 && @score_e_notation do %>
+              <%= if dir != @seat && @show_relative_scores && score >= 0 do "+" else "" end %><%= mantissa(score) %>e<b><%= exponent(score) %></b>
             <% else %>
-              <%= @score[dir] %>
+              <%= if dir != @seat && @show_relative_scores && score >= 0 do "+" else "" end %><%= score %>
             <% end %>
           </div>
         </div>
@@ -31,18 +33,36 @@ defmodule RiichiAdvancedWeb.CompassComponent do
     """
   end
 
-  def prepare_compass(kyoku, available_seats) do
+  def mantissa(score) do
+    chars = 4 + if score < 0 do 1 else 0 end - if exponent(score) >= 10 do 1 else 0 end
+    1.0 * score |> :erlang.float_to_binary() |> String.slice(0..chars)
+  end
+
+  def exponent(score) do
+    score |> abs() |> :math.log10() |> trunc()
+  end
+
+  def prepare_compass(assigns) do
     symbols = %{east: "東", south: "南", west: "西", north: "北"}
     Enum.map([:east, :south, :west, :north], fn seat -> 
-      if seat in available_seats do
-        {seat, symbols[Riichi.get_seat_wind(kyoku, seat, available_seats)]}
+      if seat in assigns.available_seats do
+        score = assigns.score[seat]
+        score = if seat != assigns.seat && assigns.show_relative_scores do score - assigns.score[assigns.seat] else score end
+        {seat, symbols[Riichi.get_seat_wind(assigns.kyoku, seat, assigns.available_seats)], score}
       else
-        {seat, ""}
+        {seat, "", 0}
       end
     end)
   end
 
-  def handle_event("notify_ai", _assigns, socket) do
+  def handle_event("compass_clicked", _assigns, socket) do
+    socket = assign(socket, :show_relative_scores, true)
+    if socket.assigns.show_relative_scores_timer != nil do
+      Process.cancel_timer(socket.assigns.show_relative_scores_timer)
+    end
+    timer = send_update_after(self(), __MODULE__, [id: "compass", show_relative_scores: false, show_relative_scores_timer: nil], 2000)
+    socket = assign(socket, :show_relative_scores_timer, timer)
+    # notify ai
     GenServer.cast(socket.assigns.game_state, :notify_ai)
     {:noreply, socket}
   end
