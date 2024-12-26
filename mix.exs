77c8b51d931f6d2a527fabeeef0528189a4f358c
Daniel Wang <danielwang9@protonmail.com>
2024-12-26 04:41:26 -0600

    Bump phoenix version

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule RiichiAdvanced.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.14"},
+      {:phoenix, "~> 1.7.18"},
       # even though we don't use the actual database,
       # we still use Ecto.UUID.generate() some places
       {:phoenix_ecto, "~> 4.5"},
@@ -41,7 +41,7 @@ defmodule RiichiAdvanced.MixProject do
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       # TODO bump on release to {:phoenix_live_view, "~> 1.0.0"},
-      {:phoenix_live_view, "~> 1.0.0-rc.1", override: true},
+      {:phoenix_live_view, "~> 1.0.1"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
