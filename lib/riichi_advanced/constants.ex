5b2cc77bdb1f8d33e4502ca0c359e285e8e6ecc7
Daniel Wang <danielwang9@protonmail.com>
2025-02-04 22:49:54 -0600

    Bump to v1.0.2

--- lib/riichi_advanced/constants.ex
+++ lib/riichi_advanced/constants.ex
@@ -1,6 +1,6 @@
 defmodule RiichiAdvanced.Constants do
 
-  @version "v1.0.1." <> (System.cmd("git", ["rev-parse", "--short", "HEAD"]) |> elem(0) |> String.trim())
+  @version "v1.0.2." <> (System.cmd("git", ["rev-parse", "--short", "HEAD"]) |> elem(0) |> String.trim())
 
   def version, do: @version
 end
