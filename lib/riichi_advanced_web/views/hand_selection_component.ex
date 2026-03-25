6a89bc257eb1d0877f3a848fa19807df88dfd2a4
Dani <itsdani@tutamail.com>
2026-03-24 22:55:42 -0700

    Make scoring_test work with attribute tiles

--- lib/riichi_advanced_web/views/hand_selection_component.ex
+++ lib/riichi_advanced_web/views/hand_selection_component.ex
@@ -1,4 +1,5 @@
 defmodule RiichiAdvancedWeb.HandSelectionComponent do
+  alias RiichiAdvanced.Utils, as: Utils
   use RiichiAdvancedWeb, :live_component
 
   def mount(socket) do
@@ -13,7 +14,7 @@ defmodule RiichiAdvancedWeb.HandSelectionComponent do
         Hand:
         <div class="hand-selection-hand">
           <%= for {tile, i} <- Enum.with_index(@hand) do %>
-            <button type="button" phx-cancellable-click="remove_hand_tile" phx-value-index={i}><div class={"tile flat #{tile}"}></div></button>
+            <button type="button" phx-cancellable-click="remove_hand_tile" phx-value-index={i}><div class={tile |> Utils.to_tile() |> Utils.get_tile_class(-1, %{}, ["flat"])}></div></button>
           <% end %>
           <%= if length(@hand) < 14 do %>
             <%= for _i <- length(@hand)..13 do %>
@@ -23,7 +24,7 @@ defmodule RiichiAdvancedWeb.HandSelectionComponent do
         </div>
         <div class="hand-selection-buttons">
           <%= for tile <- @tiles do %>
-            <button type="button" phx-cancellable-click="add_hand_tile" phx-value-tile={tile}><div class={"tile flat #{tile}"}></div></button>
+            <button type="button" phx-cancellable-click="add_hand_tile" phx-value-tile={tile}><div class={tile |> Utils.to_tile() |> Utils.get_tile_class(-1, %{}, ["flat"])}></div></button>
           <% end %>
         </div>
       </div>
