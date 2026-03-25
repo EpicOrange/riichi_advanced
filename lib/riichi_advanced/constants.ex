20b91c17d47e0f6a094557efb41d34a433fb2fe8
Dani <itsdani@tutamail.com>
2026-03-24 22:55:42 -0700

    Rewrite tianjin in majs

--- lib/riichi_advanced/constants.ex
+++ lib/riichi_advanced/constants.ex
@@ -498,12 +498,12 @@ defmodule RiichiAdvanced.Constants do
         "classical_fu",
       ]
     },
-    # currently in the process of translating these to majs
-    # this is done by using the json ruleset as a base, and applying a majs mod
-    # which will later become the ruleset
     "tianjin" => %{
       ruleset: "tianjin",
-      mods: ["tianjin"],
+      mods: [
+        "global_mods",
+        "default_auto_buttons",
+      ],
     },
   }
 
